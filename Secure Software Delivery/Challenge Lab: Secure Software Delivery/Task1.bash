ba674329b53856d8b0a9394c9e3019289c4bd40d
RIN <128571571+RINz-HCMUS@users.noreply.github.com>
2025-10-08 11:34:55 +0700

    Add commands for creating Artifact Registry repositories
    Final version

--- Secure Software Delivery/Challenge Lab: Secure Software Delivery/Task1.bash
+++ Secure Software Delivery/Challenge Lab: Secure Software Delivery/Task1.bash
@@ -16,3 +16,21 @@ gcloud services enable \
 # 2. Run the following command to download the sample Python, Docker, and Cloud Build files:
 mkdir sample-app && cd sample-app
 gcloud storage cp gs://spls/gsp521/* .
+
+# 3. Create Artifact Registry Repositories
+
+# Set your project ID as a variable for convenience
+export PROJECT_ID=$(your-project-ID)
+export REGION="your-region"
+
+# Create the scanning repository
+gcloud artifacts repositories create artifact-scanning-repo \
+  --repository-format=docker \
+  --location=$REGION \
+  --description="Docker repository for image scanning"
+
+# Create the production repository
+gcloud artifacts repositories create artifact-prod-repo \
+  --repository-format=docker \
+  --location=$REGION \
+  --description="Docker repository for production images"
